ea3609003d25376534c8e12364e369be3eaa678b
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-01-25 17:31:07 -0300

    requesito 5

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">{ planetName }</p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+const { string } = PropTypes;
+PlanetCard.propTypes = {
+  planetName: string.isRequired,
+  planetImage: string.isRequired,
+};
+
+export default PlanetCard;
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -4,10 +4,9 @@ import Title from './Title';
 class SolarSystem extends React.Component {
   render() {
     return (
-      <>
-        <div data-testid="solar-system"> </div>
+      <div data-testid="solar-system">
         <Title headline="Planetas" />
-      </>
+      </div>
     );
   }
 }
