ddf375e8bfa82b238d0dd46becaee323039b8d65
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-01-26 14:43:21 -0300

    requesito

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <section className="missions">
+          {missions.map((mission) => {
+            const { name, year, country, destination } = mission;
+            return (
+              <MissionCard
+                key={ name }
+                name={ name }
+                year={ year }
+                country={ country }
+                destination={ destination }
+              />
+            );
+          })}
+        </section>
       </div>
-
     );
   }
 }
